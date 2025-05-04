ea5960aa2df55380953eb8ba2de61071b6dde91b
Thiago Cardoso <thiagoncc@gmail.com>
2025-05-04 14:18:59 -0300

    removing special chars

--- printer/model.py
+++ printer/model.py
@@ -1,10 +1,22 @@
-import base64
+import unicodedata
 from pydantic import BaseModel, ConfigDict, field_validator
 
 class Message(BaseModel):
     title: str
     img: bytes
     msg: str
+
+    def strip_accents(self, text: str) -> str:
+        text = unicodedata.normalize('NFD', text)
+        return ''.join([c for c in text if not unicodedata.combining(c)])
+
+    @property
+    def title_ascii(self) -> str:
+        return self.strip_accents(self.title)
+
+    @property
+    def msg_ascii(self) -> str:
+        return self.strip_accents(self.msg)
 
     model_config = ConfigDict(
         ser_json_bytes="base64",
--- printer/test.py
+++ printer/test.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 from escpos.printer import Usb
 from pathlib import Path
+#from printer.profile import KNUP_PROFILE
 
 VENDOR_ID = 0x6868
 PRODUCT_ID = 0x0200
 IN_EP = 0x81
 OUT_EP = 0x03
-PROFILE = "NT-5890K"
+#PROFILE = "NT-5890K"
+PROFILE = "POS-5890"
 
 class Message(BaseModel):
     msg: str
@@ -15,18 +17,22 @@ class Message(BaseModel):
 try:
     # Initialize USB printer
     printer = Usb(VENDOR_ID, PRODUCT_ID, in_ep=IN_EP, out_ep=OUT_EP, profile=PROFILE)
+    printer.charcode("CP850")
     
     # Print text
-    printer.text("Hello, Knup Printer!\n")
-    printer.text("This is a test print.\n")
+    #printer.text("Hello, Knup Printer!\n")
+    #printer.text("This is a test print.\n")
+    printer.text("tésté çom acéntò\n")
+    printer.text("tésté çom acéntò\n")
+    printer.text("tésté çom acéntò\n")
     
     # Optional: Print a barcode
-    printer.barcode("123456789012", "EAN13")
+    #printer.barcode("123456789012", "EAN13")
 
-    printer.image("download.png", center=True)
+    #printer.image("download.png", center=True)
     
     # Cut the paper (if supported)
-    printer.cut()
+    #printer.cut()
     
     # Close the connection
     printer.close()
